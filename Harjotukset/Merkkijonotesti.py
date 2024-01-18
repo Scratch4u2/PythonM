7b412ed062461eb26bf09b19503e9d3856bf0ee3
eemilnu <eemil.nurmi@gmail.com>
2024-01-18 14:48:57 +0200

    Tunnin esimerkkejä

---
+++ Harjotukset/Merkkijonotesti.py
@@ -0,0 +1,3 @@
+#luku = input("Talon numero?\n"), print(f"Asun Kuninkaantie " + {luku})
+
+print("Asun Kuninkaantie " + input("Talon numero?\n"))
---
+++ Harjotukset/Muuntajatesti.py
@@ -0,0 +1,2 @@
+tuumat = float(input("Anna mitta tuumina: \n "))
+print(f"Sentteiä: {tuumat * 2.54}")
