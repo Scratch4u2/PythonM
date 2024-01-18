ed69f4a9131c9919c6e787aac4109411108abcb8
eemilnu <eemil.nurmi@gmail.com>
2024-01-18 12:43:37 +0200

    tht 4 valmis ja tht 5 luonti

---
+++ Ohjelmisto-1-PModuulit/Python - Moduuli 2 tehtävät/Tehtävä 4.py
@@ -0,0 +1,7 @@
+i = int(input("Ensimmäinen numero.\n"))
+j = int(input("Toinen numero.\n"))
+k = int(input("Kolmas numero.\n"))
+
+print(f"Summa on {i + j + k} ")
+print(f"Tulo on {i * j * k} ")
+print(f"Keskiarvo on {(i * j *k)/2} ")
