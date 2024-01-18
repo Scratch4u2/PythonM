3042295e4990d00e90791bf02a9811fffd9c16a0
eemilnu <eemil.nurmi@gmail.com>
2024-01-18 12:32:19 +0200

    tht 3 valmis ja tht 4 luonti

---
+++ Ohjelmisto-1-PModuulit/Python - Moduuli 2 tehtävät/Tehtävä 3.py
@@ -0,0 +1,5 @@
+kanta = int(input("Kannan pituus.\n"))
+korkeus = int(input("Korkeus. \n"))
+
+print(f"Suorakulmion pinta-ala on {kanta * korkeus}")
+print(f"Suorakulmion piirin pituus on {2*kanta + 2*korkeus} ")
