eeed0736c6ee02730fdc7ab5fd5c6aa3f79f2a95
eemilnu <eemil.nurmi@gmail.com>
2024-01-25 13:40:05 +0200

    tht 34 valmis ja moduuli 3 valmis

---
+++ Ohjelmisto-1-PModuulit/Python - Moduuli 3 tehtävät/Tehtävä 4.py
@@ -0,0 +1,15 @@
+vuosi = int(input("Mikä vuosi: \n"))
+
+if vuosi < 100:
+    if vuosi % 4 == 0 and vuosi % 100 != 0:
+        print(f"Vuosi {vuosi} on karkausvuosi")
+    else:
+        print(f"Vuosi {vuosi} ei ole karkausvuosi")
+
+elif vuosi >= 100:
+    if vuosi % 4 == 0 and vuosi % 400 == 0:
+        print(f"Vuosi {vuosi} on karkausvuosi")
+    else:
+        print(f"Vuosi {vuosi} ei ole karkausvuosi")
+
+
