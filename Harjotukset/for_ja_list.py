ffcba0677254a336d09e4cd80e83fbe28aa1e044
eemilnu <eemil.nurmi@gmail.com>
2024-01-29 13:53:27 +0200

    harjoitus

--- Harjotukset/for_ja_list.py
+++ Harjotukset/for_ja_list.py
@@ -6,10 +6,10 @@ ihminen5 = "Jasmin"
 ihminen6 = "Jimmy"
 
 ihmiset = [ihminen1, ihminen2, ihminen3, ihminen4, ihminen5, ihminen6]
-ihmiset2 = ["John", "James"]
+#ihmiset2 = ["John", "James"]
 
-ihmiset.extend(ihmiset2)
-
+#ihmiset.extend(ihmiset2)
+ihmiset.append("Matti")
 #ihmiset.append(input("Anna nimi: \n"))
 print(ihmiset)
 
