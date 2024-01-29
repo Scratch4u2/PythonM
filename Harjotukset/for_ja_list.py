2e7fea004c3dd8cebb6f7d2af51e60dc2faeb6be
eemilnu <eemil.nurmi@gmail.com>
2024-01-29 13:20:59 +0200

    harjoitus

---
+++ Harjotukset/for_ja_list.py
@@ -0,0 +1,10 @@
+ihminen1 = "Juha"
+ihminen2 = "Jarkko"
+ihminen3 = "Jussi"
+ihminen4 = "Juhani"
+ihminen5 = "Jasmin"
+ihminen6 = "Jimmy"
+
+ihmiset = [ihminen1, ihminen2, ihminen3, ihminen4, ihminen5, ihminen6]
+
+print(ihmiset[1:5:2])
