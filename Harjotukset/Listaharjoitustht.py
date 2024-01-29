a2efa5b32edfc36db0f32f3c8201d588748a42ba
eemilnu <eemil.nurmi@gmail.com>
2024-01-29 14:17:04 +0200

    harjoitus

--- Harjotukset/Listaharjoitustht.py
+++ Harjotukset/Listaharjoitustht.py
@@ -1,8 +1,18 @@
-items = [1,3,4,5,6,9,10,17,23,24]
+"""items = [1,3,4,5,6,9,10,17,23,24]
 result = []
 
 for i in items:
     if i % 2 != 0:
         result.append(i)
 
-print(result)
+print(result)"""
+
+probability = [0.52, 0.49, 0.72, 0.01, 0.12, 0.11]
+results = []
+
+for p in probability:
+    if p >= 0.5:
+        results.append(1)
+    else:
+        results.append(0)
+print(results)
