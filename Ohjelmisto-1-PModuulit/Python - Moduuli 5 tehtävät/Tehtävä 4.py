dae013f022f7e2e492bb71f2a61d2e0c507a7b9e
eemilnu <eemil.nurmi@gmail.com>
2024-01-30 14:09:21 +0200

    tht4 valmis

---
+++ Ohjelmisto-1-PModuulit/Python - Moduuli 5 tehtävät/Tehtävä 4.py
@@ -0,0 +1,11 @@
+kaupungit = []
+
+
+for k in range(0,5):
+    u_kaupunki = input("Anna kaupungin nimi: \n")
+    kaupungit.append(u_kaupunki)
+    k += 1
+
+for i in range(0,5):
+    print(kaupungit[i])
+    i += 1
