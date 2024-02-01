a71e2ca2d030b4856f1c9bf03cb4b1afc6b7c402
eemilnu <eemil.nurmi@gmail.com>
2024-02-01 14:47:34 +0200

    harjoitus

---
+++ Harjotukset/ListaTuple.py
@@ -0,0 +1,16 @@
+"""lista1 = ["John", 1,5,3,1,"John",True,"John"]
+lista2 = []
+
+for item in lista1:
+    if type(item) == bool or item not in lista2:
+        lista2.append(item)
+
+print(lista2)"""
+
+lista = [1,2,3,5]
+lista1 = [4,[6,8],9]
+lista2 = []
+
+lista2.extend(lista)
+lista2.extend(lista1)
+print(lista2)
