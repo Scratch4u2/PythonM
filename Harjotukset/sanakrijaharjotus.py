182cd5880eb5f50dc49cb4da780bd829f32b76ca
eemilnu <eemil.nurmi@gmail.com>
2024-02-05 13:32:33 +0200

    harjoitus

--- Harjotukset/sanakrijaharjotus.py
+++ Harjotukset/sanakrijaharjotus.py
@@ -1,3 +1,4 @@
+"""
 numbers = {"Viivi": "122345"
            , "Joni": "12142356"
            , "Kalle": "124224525"}
@@ -8,3 +9,9 @@ print(numbers)
 name = input("What is your name?: \n ")
 if name in numbers:
     print(f"Hello, {name}´s number is {numbers[name]}")
+"""
+
+numbers ={}
+for i in range(1,10):
+    numbers[i] = i**2
+print(numbers)
