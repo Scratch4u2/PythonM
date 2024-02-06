de9a0af5c1da089939c5c7535bf886998da00e20
eemilnu <eemil.nurmi@gmail.com>
2024-02-06 10:53:52 +0200

    harjoitus

--- Harjotukset/sqltesti.py
+++ Harjotukset/sqltesti.py
@@ -8,15 +8,25 @@ connection = mysql.connector.connect(
     password="root",
     autocommit=True
 )
-
+""""
 cursor = connection.cursor()
 cursor.execute("SELECT iso_country, name FROM country WHERE name='Finland' ")
 result_row = cursor.fetchall()
-#print(f"Ensimmäinen rivi: {result_row[0]}, maakoodi: {result_row[0][0]}")
+print(f"Ensimmäinen rivi: {result_row[0]}, maakoodi: {result_row[0][0]}")
 print(f"Maakoodeja löytyi {cursor.rowcount} kappaletta.")
 if cursor.rowcount > 0:
     for row in result_row:
         print(f"maakoodi: {row[0]}, nimi: {row[1]}")
     else:
-        print("ei tuloksia")
+        print("ei tuloksia")"""
 
+def find_country_by_code(iso_code):
+    sql = f"SELECT name, continent FROM country WHERE iso_country= '{iso_code}'"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchone()
+    if cursor.rowcount == 1:
+        return result
+
+country = find_country_by_code("FI")
+print(country)
