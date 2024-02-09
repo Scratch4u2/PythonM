49eea104c29ff08d9986a3379d8a6d3ad0fab030
eemilnu <eemil.nurmi@gmail.com>
2024-02-09 12:03:37 +0200

    teht 2 valmis tht 3 luonti

--- Ohjelmisto-1-PModuulit/Python - Moduuli 8 tehtävät/Tehtävä 2.py
+++ Ohjelmisto-1-PModuulit/Python - Moduuli 8 tehtävät/Tehtävä 2.py
@@ -10,16 +10,29 @@ connection = mysql.connector.connect(
 )
 
 def kenttatyyppi(user_input):
-    sql = f"SELECT type FROM airport WHERE iso_country= '{user_input}'"
+    sql = f"SELECT type FROM airport WHERE iso_country= '{user_input}' ORDER BY type"
     cursor = connection.cursor()
     cursor.execute(sql)
-    result = cursor.fetchall()
-    if cursor.rowcount:
-        return result
+    rivit = cursor.fetchall()
+    montako = {
+        "closed": 0,
+        "heliport": 0,
+        "large_airport": 0,
+        "medium_airport": 0,
+        "small_airport": 0,
+        "seaplane_base": 0,
+        "balloonport": 0
+    }
+    for rivi in rivit:
+        tyyppi = rivi[0]
+        if tyyppi in montako:
+            montako[tyyppi] += 1
+    return montako
+
 
 user_input = input("Anna maakoodi: \n").upper()
-airport = find_country_by_code(user_input)
-if airport != None:
-    print(airport)
+kenttatyypit = kenttatyyppi(user_input)
+if kenttatyypit != None:
+    print(kenttatyypit)
 else:
     print("Ei tuloksia")
