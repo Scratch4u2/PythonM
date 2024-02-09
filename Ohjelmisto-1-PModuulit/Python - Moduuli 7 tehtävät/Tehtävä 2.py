a3c8967fe0ca5dddbc116c03ca6b9bd8cb73a90a
eemilnu <eemil.nurmi@gmail.com>
2024-02-09 11:00:33 +0200

    teht 2 valmis ja tht 3 luonti

--- Ohjelmisto-1-PModuulit/Python - Moduuli 7 tehtävät/Tehtävä 2.py
+++ Ohjelmisto-1-PModuulit/Python - Moduuli 7 tehtävät/Tehtävä 2.py
@@ -1,1 +1,10 @@
-fe
+nimi = input("Anna nimi (Tyhjä merkkijono lopettaa kyselyn): \n")
+nimet = set()
+while nimi !="":
+    if nimi in nimet:
+        print("Aiemmin syötetty nimi")
+    else:
+        print("Uusi nimi")
+        nimet.add(nimi)
+    nimi = input("Anna nimi (Tyhjä merkkijono lopettaa kyselyn): \n")
+print(nimet)
